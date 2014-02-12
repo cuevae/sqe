78fc4a28eee135771cef0ca7a4f641b90b08daf3
cuevaec <cuevaec@gmail.com>
2014-02-12 23:10:38 +0000

    testing the mvc framework

---
+++ app/views/templates/footer.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: manei
+ * Date: 12/02/14
+ * Time: 18:21
+ */
+?>
+<strong>&copy; SQE 2014</strong></body></html>
---
+++ app/views/templates/header.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: manei
+ * Date: 12/02/14
+ * Time: 18:19
+ */
+?>
+<html>
+<head>
+    <title><?= $title ?></title>
+</head>
+<body>
+    <h1>EmployMe</h1>
