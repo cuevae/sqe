38f8951fdd7b9a3cd778491540ab2c87e8b514ec
cuevaec <cuevaec@gmail.com>
2014-03-18 21:51:21 +0000

    skills add and test

--- app/models/skills.php
+++ app/models/skills.php
@@ -11,7 +11,7 @@ class Skills_Model extends CI_Model
 
     public function __construct()
     {
-        parent::_construct();
+        parent::__construct();
         $this->load->database();
     }
 
@@ -20,9 +20,25 @@ class Skills_Model extends CI_Model
 
     }
 
-    public function addSkill( $skillName, $skillLevel )
+    public function addSkill( $idUser, $skillName, $skillLevel, $verified = null, $howVerified = null )
     {
+        $data = [ 'Persons_idUser' => $idUser, 'skillName' => $skillName, 'skillLevel' => $skillLevel ];
 
+        if ( !empty( $verified ) ) {
+            $data['verified'] = ( $verified == true );
+        }
+
+        if ( !empty( $howVerified ) ) {
+            $data['howVerified'] = $howVerified;
+        }
+
+        try {
+            $this->db->insert( 'skills', $data );
+        } catch ( Exception $e ) {
+            return false;
+        }
+
+        return $this->db->insert_id();
     }
 
     public function deleteSkill( $skillId )
--- tests/models/SkillsModelTest.php
+++ tests/models/SkillsModelTest.php
@@ -12,9 +12,17 @@ class SkillsModelTest extends CIUnit_TestCase
     protected $model;
     protected $availableSkillLevels = array( 'Basic', 'Good', 'Excellent' );
 
+    public function setUp()
+    {
+        $this->CI->load->model( 'skills' );
+        $this->model = $this->CI->skills;
+    }
+
     public function testAddSkill()
     {
-
+        /*$res = $this->model->addSkill( 1, 'Java', 'Excellent' );
+        $this->assertNotEmpty( $res );
+        $this->assertTrue( $res > 0 );*/
     }
 
 } 
