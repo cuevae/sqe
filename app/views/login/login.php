b80c72c6fafac960ce8d42f97ee78bf882ff91d3
cuevaec <cuevaec@gmail.com>
2014-03-23 07:47:00 +0000

    signup link from the login page

--- app/views/login/login.php
+++ app/views/login/login.php
@@ -1,24 +1,24 @@
 <div id="login_form" style="width:400px; margin:100px auto; border-radius: 5px;border:1px solid #909090; padding:20px">
 
     <?php
-    if (!empty ($error_message)) {
+    if ( !empty ( $error_message ) ) {
         echo '<span style="color:red">' . $error_message . '</span>';
 
     }
-    echo form_open('login');
+    echo form_open( 'login' );
 
-    echo form_label('Username :') . br(2);
+    echo form_label( 'Username :' ) . br( 2 );
     $data = array(
         'name' => 'username',
         'id' => 'username',
-        'value' => set_value('user_name'),
+        'value' => set_value( 'user_name' ),
         'style' => 'width:100%',
     );
-    echo form_input($data) . br(2);
-    echo form_error('username');
+    echo form_input( $data ) . br( 2 );
+    echo form_error( 'username' );
 
 
-    echo form_label('Password: ') . br(2);
+    echo form_label( 'Password: ' ) . br( 2 );
     $data = array(
         'name' => 'password',
         'id' => 'Password',
@@ -26,8 +26,8 @@
         'style' => 'width:100%',
 
     );
-    echo form_password($data) . br(2);
-    echo form_error('password');
+    echo form_password( $data ) . br( 2 );
+    echo form_error( 'password' );
     $data = array(
         'name' => 'Login',
         'id' => 'login',
@@ -35,9 +35,11 @@
         'style' => 'width:40%',
 
     );
-    echo form_submit($data) . br(2);
+    echo form_submit( $data ) . br( 2 );
 
     echo form_close();
     ?>
+
+    <p>Or <a href="<?= site_url( 'signup' ) ?>">sign up</a></p>
 </div>
 
